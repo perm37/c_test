eaa14538905d6601a6d9f9ae274bfe277184dd8f
Jon Brown <jbrown@rackspace.com>
2015-06-23 07:42:11 -0500

    completed ex1-23.c

--- ex1-23.c
+++ ex1-23.c
@@ -10,63 +10,122 @@
 #define MAXLINE 1000
 void populate(char comment[], int comm);
 void remove_comm(char comment[]);
+void init_arr(char comment[]);
 
 int main()
 {
+/*printf("beginning of main function\n");*/
  int c;
- while(c != EOF){
+ int i;
  char comment[MAXLINE];
+ while(c != EOF){
+/*printf("while c != EOF\n");*/
+ init_arr(comment);
  populate(comment, c);
- remove_comm(
-
+ remove_comm(comment);
+/*printf("in main function\n");*/
+ c = 1;
+/* printf("c is %c\n", c);*/
+ for ( i = 0; c != '\0' ; ++i  ){
+/*printf("  for ( i = 0; c != \\0; ++i )\n");*/
+  c = comment[i];
+  putchar(c);  
+ 
  }
+putchar('\n');
+init_arr(comment);
+}
 }
 
-void populate(char comment[],int c){
+/*function to populate array with characters from user input*/
+void populate(char comment[], int c){
+/*printf("in populate function\n");*/
   int i;
-  while((c = getchar()) != EOF){
-   for(i = 0; c != '\n'; ++i){
+   for(i = 0; (c = getchar()) != '\n' || '\0'; ++i){
      comment[i] = c;
    }
-    comment[(++i)] = '\n';
-    comment[(++i)] = '\0';
-  }
+    comment[i] = '\n';
+    ++i;
+    comment[i] = '\0';
 return;
 }    
 
-   
+/*function to initialize the array*/
+void init_arr(char comment[]){
+ int i;
+ for (i = 0; i < MAXLINE; ++i){
+   comment[i] = 0;
+ }
+}
+
+
+
+/*function to remove all comments*/   
 void remove_comm(char comment[]){
+/*printf("in remove_comm function\n");*/
   int i;
   int c;
   int f_comm = 0;
   int l_comm = 0;
-  f_comm_pos = 0;
-  l_comm_pos = 0;
+  int f_comm_pos = 0;
+  int l_comm_pos = 0;
   int lock;
 
-  for(i = 0; c != EOF; ++i){
+  for(i = 0; (c = comment[i]) != '\0'; ++i){
+  /*for(i = 0; c != '\0'; ++i){*/
+/*printf("for i =0; c != \\0; ++i\n");*/
    c = comment[i];
-    if (c == '/'){
+/*printf("c is %c\n", c);*/
+/*printf("f_comm is %d\n", f_comm);*/
+    if (c == '/' && f_comm != 1){
+/*printf("  if c == // && f_comm != 1\n");*/
        lock = 1;
        while( lock == 1 && f_comm != 1 ){
+/*printf("    while lock == 1 &&  f_comm != 1\n");*/
          if ((c = comment[++i]) == '*'){
+/*printf("      if (c = comment[++i]) == *\n ");*/
            f_comm = 1;
-           f_comm_pos = i;
+           f_comm_pos = i - 1;
            c = comment[++i];
          }
          else
+/*printf("if /(c = comment[++i]/) == *  ---- else lock=0;\n");*/
           lock = 0;
        }
-     if (f_comm == 1 && c == '*'){
+      }
+     if (c == '*' && f_comm == 1 && l_comm != 1){
+/*printf("if (c == '*' && f_comm == 1 && l_comm != 1\n");*/
        lock = 1;
        while ( lock == 1 && l_comm != 1){
-         if (( c = comment[++i]) == '/'){
+/*printf("  while ( lock == 1 && l_comm != 1)\n");*/
+         if (( c = comment[(++i)]) == '/'){
+/*printf("    if (( c = comment[(++i)]) == // )\n");*/
              l_comm = 1;
-             l_comm_pos = i;
+             l_comm_pos = i - 1;
          }
          else
+/*printf("    if (( c = comment[(++i)]) == // ) --- else lock = 0\n");*/
           lock = 0;
         }
      }
-    if (f_com == 1 && l_comm == 1){
-      }
+    if (f_comm == 1 && l_comm == 1){
+/*printf("if (f_comm == 1 && l_comm == 1)\n");*/
+       
+       comment[l_comm_pos] = ' ';
+/*printf("l_comm_pos = %d\n", l_comm_pos);*/
+       comment[(l_comm_pos + 1)] = ' ';
+/*printf("l_comm_pos = %d\n", l_comm_pos);*/
+       comment[f_comm_pos] = ' ';
+/*printf("f_comm_pos = %d\n", f_comm_pos);*/
+       comment[(f_comm_pos + 1)] = ' ';
+/*printf("f_comm_pos = %d\n", f_comm_pos);*/
+
+f_comm_pos = 0;
+f_comm = 0;
+l_comm_pos = 0;
+l_comm = 0;
+
+    }   
+ }
+return;
+}
